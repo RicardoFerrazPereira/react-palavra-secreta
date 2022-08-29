acc92317a7eb25bf65212b44d3772fa2affc3b32
RicardoFerraz <sourvivens@gmail.com>
2022-08-29 11:33:05 -0300

    Criando os componentes do projeto

--- secretword/src/App.js
+++ secretword/src/App.js
@@ -1,10 +1,36 @@
+//CSS
 import './App.css';
+// React
+import { useCallback, useEffect, useState } from "react";
+// Dados
+import { wordsList } from "./data/words";
+// Componentes
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+// estágios do jogo - usado pra fazer a progressão
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+]
 
 function App() {
+  // iniciar no estágio 1 ('start')
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  // iniciar os dados
+  const [words] = useState(wordsList);
+
+  console.log(words);
+
+
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <Game />}
+      {gameStage === "end" && <GameOver />}
+
     </div>
   );
 }
--- secretword/src/components/StartScreen.js
+++ secretword/src/components/StartScreen.js
@@ -3,7 +3,7 @@ import "./StartScreen.css";
 const StartScreen = () => {
   return (
     <div className="start">
-      <h1>Secret Word</h1>
+      <h1>Palavra Secreta</h1>
       <p>Clique no botão abaixo parta começar a jogar</p>
       <button>Começar o jogo</button>
     </div>
